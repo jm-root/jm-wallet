a6030e881d5722a8cd1a96869dcd1854be89f310
jeff <jeff@jamma.cn>
2017-08-18 18:25:52 +0800

    create

---
+++ config/index.js
@@ -0,0 +1,31 @@
+require('log4js').configure(require('path').join(__dirname, 'log4js.json'))
+var config = {
+  development: {
+    port: 3000,
+    lng: 'zh_CN',
+    gateway: 'http://api.mx.jamma.cn',
+    ctCode: 'tb',
+    modules: {
+      'wallet': {
+        module: process.cwd() + '/lib'
+      }
+    }
+  },
+  production: {
+    port: 3000,
+    lng: 'zh_CN',
+    gateway: 'http://gateway.app',
+    ctCode: 'tb',
+    modules: {
+      'wallet': {
+        module: process.cwd() + '/lib'
+      }
+    }
+  }
+}
+
+var env = process.env.NODE_ENV || 'development'
+config = config[env] || config['development']
+config.env = env
+
+module.exports = config
